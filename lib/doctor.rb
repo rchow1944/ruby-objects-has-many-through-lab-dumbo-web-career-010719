77cf2ce4d6830b1553ee76a95384de9b4a0f8017
rchow1944 <rchow2@buffalo.edu>
2019-01-11 11:50:56 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Doctor
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select {|appt| appt.doctor == self}
+  end
+
+  def patients
+    # binding.pry
+    self.appointments.map {|appt| appt.patient}
+  end
+end
---
+++ lib/patient.rb
@@ -0,0 +1,26 @@
+class Patient
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select {|appt| appt.patient == self}
+  end
+
+  def doctors
+    self.appointments.map {|appt| appt.doctor}
+  end
+
+  def self.all
+    @@all
+  end
+end
